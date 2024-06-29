32da37d6aa3e826d01268cbfff69de1b2b3a7576
Fresh Project <blayobi@gmail.com>
2024-06-29 15:58:04 +0100

    Placed home screen in it's own file

---
+++ lib/home_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(children: [
+//Flex wrapper for the Gmail logo starts here..
+          Expanded(
+            child: Container(
+              color: Colors.white,
+              child: Stack(
+                children: [
+                  Container(
+                    width: 350.0,
+                    height: 200.0,
+//alignment: Alignment.bottomCenter,
+                    margin: EdgeInsets.only(top: 325.0),
+//color: Colors.indigo,
+                    child: Image.asset(
+                      'assets/gmail_img.png',
+                      alignment: Alignment.bottomCenter,
+                    ),
+                  ),
+
+                  Container(
+                    width: 350.0,
+                    height: 200.0,
+                    margin: EdgeInsets.only(top: 460.0),
+                    alignment: Alignment.topCenter,
+//color: Colors.red,
+                    child: Text(
+                      'Gmail',
+                      style: TextStyle(
+                        fontSize: 42.0,
+//fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+
+//the Gmail text starts here..
+                ],
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,58 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      body: Center(
-        child: Column(children: [
-          //Flex wrapper for the Gmail logo starts here..
-          Expanded(
-            child: Container(
-              color: Colors.white,
-              child: Stack(
-
-                children: [
-
-                  Container(
-                    width: 350.0,
-                    height: 200.0,
-                    //alignment: Alignment.bottomCenter,
-                    margin: EdgeInsets.only(top: 325.0),
-                    //color: Colors.indigo,
-                    child: Image.asset(
-                      'assets/gmail_img.png',
-                       alignment: Alignment.bottomCenter,
-                    ),
-                  ),
-
-                  Container(
-                    width: 350.0,
-                    height: 200.0,
-                    margin: EdgeInsets.only(top: 460.0),
-                    alignment: Alignment.topCenter,
-                    //color: Colors.red,
-                    child: Text(
-                      'Gmail',
-                      style: TextStyle(
-                        fontSize: 42.0,
-                        //fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-
-
-
-
-
-
-                  //the Gmail text starts here..
-
-
-                ],
-              ),
-            ),
-          ),
-        ]),
-      ),
-    ));
+      home: HomeScreen(),
+    );
   }
 }
