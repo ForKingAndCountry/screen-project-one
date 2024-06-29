c51670a82ed4bf4be5ce098978c45a8d9684a01d
Fresh Project <blayobi@gmail.com>
2024-06-29 16:46:54 +0100

    Name Change to splash screen & home screen started

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -4,47 +4,23 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        leading: Icon(
+          Icons.menu,
+        ),
+        title: SearchBar(
+          
+        ),
+      ),
       body: Center(
-        child: Column(children: [
-//Flex wrapper for the Gmail logo starts here..
-          Expanded(
-            child: Container(
-              color: Colors.white,
-              child: Stack(
-                children: [
-                  Container(
-                    width: 350.0,
-                    height: 200.0,
-//alignment: Alignment.bottomCenter,
-                    margin: EdgeInsets.only(top: 325.0),
-//color: Colors.indigo,
-                    child: Image.asset(
-                      'assets/gmail_img.png',
-                      alignment: Alignment.bottomCenter,
-                    ),
-                  ),
-
-                  Container(
-                    width: 350.0,
-                    height: 200.0,
-                    margin: EdgeInsets.only(top: 460.0),
-                    alignment: Alignment.topCenter,
-//color: Colors.red,
-                    child: Text(
-                      'Gmail',
-                      style: TextStyle(
-                        fontSize: 42.0,
-//fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-
-//the Gmail text starts here..
-                ],
-              ),
-            ),
-          ),
-        ]),
+        child: Row(
+          children: [
+            Container(
+              color: Colors.grey,
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'splash_screen.dart';
 import 'home_screen.dart';
 
 void main() {
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: HomeScreen(),
+
     );
   }
 }
